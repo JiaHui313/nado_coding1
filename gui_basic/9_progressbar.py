2b9caa7e898b8fa6e96c7265494ff253135a5596
Jiahui313 <leegh8003@naver.com>
2020-09-21 15:13:36 +0900

    프로그레스바(진행바)

---
+++ gui_basic/9_progressbar.py
@@ -0,0 +1,36 @@
+import time
+import tkinter.ttk as ttk
+from tkinter import *
+
+root =Tk()
+root.title("GUI")
+root.geometry("640x480") #창 크기: 가로*세로
+
+# progressbar = ttk.Progressbar(root, maximum=100, mode="indeterminate")
+# progressbar = ttk.Progressbar(root, maximum=100, mode="determinate")
+# progressbar.start(10) #10ms마다 움직임
+# progressbar.pack() #화면에 넣는 함수pack()
+
+# def btncmd():
+#     progressbar.stop() #작동중지
+
+# btn= Button(root, text="중지", command=btncmd)
+# btn.pack()
+
+p_var2 = DoubleVar()
+progressbar2 = ttk.Progressbar(root, maximum=100, length=150, variable=p_var2)
+progressbar2.pack()
+
+def btncmd2():
+    for i in range(1, 101): #1 ~ 100까지
+        time.sleep(0.01) #0.01초 대기
+
+        p_var2.set(i) #프로그레스바의 값을 설정
+        progressbar2.update() # ui 업데이트
+        print(p_var2.get())
+
+btn= Button(root, text="시작", command=btncmd2)
+btn.pack()
+
+
+root.mainloop() #창이 닫히지 않도록
